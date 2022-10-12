9185f29898644066e2ceb156a79902bbca8ca08a
Railgun <aabouleish@gmail.com>
2022-10-11 22:01:45 -0500

    breakouts

--- selenium_tests.js
+++ selenium_tests.js
@@ -20,12 +20,8 @@ async function selenTest(character) {
 
    await driver.sleep(1000);
 
-   //getting table header nodes
-   const h2Nodes = await driver.findElements(By.css("h2"));
-   const headerPromises = h2Nodes.map((elem) => elem.getText());
-
-   //creating array of table headers
-   const tableHeaders = await Promise.all(headerPromises);
+   //list of h2 elements
+   const tableHeaders = await assignTableHeaders(driver);
 
    // loop through table IDs until no more tables are found
    const tablePromises = tableHeaders.map(async (_, tableNum) => {
@@ -33,10 +29,10 @@ async function selenTest(character) {
 
       // tableHeaders.length is arbitrary. Once there are no more tables to scrape,
       // the findElement() below will throw an error when "DataTables_Table_i" doesnt exist
-      let TableNode;
+      let tableNode;
 
       try {
-         TableNode = await driver.findElement(By.id(tableID));
+         tableNode = await driver.findElement(By.id(tableID));
       } catch (error) {
          return undefined;
       }
@@ -45,50 +41,19 @@ async function selenTest(character) {
 
       characterObj[tableName] = {};
 
-      // getting column headers
-      const columnNodes = await TableNode.findElement(
-         By.css("tr")
-      ).findElements(By.css("th"));
-
       // saving column headers
-      const columnHeaders = [];
-
-      for (const node of columnNodes) {
-         const header = await node.getText();
-         columnHeaders.push(header);
-      }
+      const columnHeaders = await getColumnHeaders(tableNode);
 
       // getting all row nodes for the table
-      const rowNodes = await TableNode.findElement(
-         By.css("tbody")
-      ).findElements(By.css("tr"));
+      const rowNodes = await tableNode
+         .findElement(By.css("tbody"))
+         .findElements(By.css("tr"));
 
       // looping through each row node to save the data
-      const rowPromises = rowNodes.map(async (row) => {
-         const scrapedInfo = [];
 
-         // saving the image url (located within a css attribute, not displayed on page load)
-         const rowDetails = await row.getAttribute("data-details");
-         scrapedInfo.push(["Image", parseforHref(rowDetails)]);
-
-         // saving nodes for all cells in a row
-         const moveInfo = await row.findElements(By.css("td"));
-         let columnIndex = 0;
-
-         const textPromises = moveInfo.map((node) => node.getText());
-         const texts = await Promise.all(textPromises);
-
-         // getting text from each cell node, and saving it with the appropriate collumn header
-         for (const text of texts) {
-            if (columnIndex !== 0) {
-               scrapedInfo.push([columnHeaders[columnIndex], text]);
-            }
-            columnIndex++;
-         }
-
-         scrapedInfo.push(tableName);
-         return scrapedInfo;
-      });
+      const rowPromises = rowNodes.map((row) =>
+         scrapeRow(row, tableName, columnHeaders)
+      );
 
       const rows = await Promise.all(rowPromises);
 
@@ -108,34 +73,15 @@ async function selenTest(character) {
                ? scrapedInfo[2][1]
                : scrapedInfo[1][1];
          // adding the row to the table object
-         characterObj[scrapedInfo[scrapedInfo.length - 1]][rowObjKey] =
-            scrapedInfo;
+         const tableName = scrapedInfo.pop();
+
+         characterObj[tableName][rowObjKey] = scrapedInfo;
       }
    }
 
    await driver.quit();
    return characterObj;
 }
-
-// data-details css attribute is a string with a ton of information used when the row is expanded to display an image of the move
-// this function parses the string for the stored src attribute which contains the image url
-// each image has 2 srcs listed. the code below correctly slices the 1st of the 2
-const parseforHref = (string) => {
-   const arr = string.split(" ");
-
-   const imgURLs = [];
-   let oddURLsOnly = true;
-   for (const str of arr) {
-      if (/^src/.test(str)) {
-         if (oddURLsOnly) {
-            imgURLs.push(`https://dustloop.com${str.slice(5, str.length - 1)}`);
-         }
-         oddURLsOnly = !oddURLsOnly;
-      }
-   }
-
-   return imgURLs;
-};
 
 //CREATE SEPERATE FUNCTION FOR ABA
 
@@ -182,6 +128,69 @@ const main = async () => {
 
 main();
 
-// buid a scraper for all character names, then loop throught them
+const assignTableHeaders = async (driver) => {
+   //getting table header nodes
+   const h2Nodes = await driver.findElements(By.css("h2"));
+   const headerPromises = h2Nodes.map((elem) => elem.getText());
+
+   //creating array of table headers
+   return Promise.all(headerPromises);
+};
+
+const getColumnHeaders = async (tableNode) => {
+   const columnNodes = await tableNode
+      .findElement(By.css("tr"))
+      .findElements(By.css("th"));
+
+   const header = columnNodes.map((node) => node.getText());
+
+   return Promise.all(header);
+};
+
+const scrapeRow = async (row, tableName, columnHeaders) => {
+   const scrapedInfo = [];
+
+   // saving the image url (located within a css attribute, not displayed on page load)
+   const rowDetails = await row.getAttribute("data-details");
+   scrapedInfo.push(["Image", parseforHref(rowDetails)]);
+
+   // saving nodes for all cells in a row
+   const moveInfo = await row.findElements(By.css("td"));
+   let columnIndex = 0;
+
+   const textPromises = moveInfo.map((node) => node.getText());
+   const texts = await Promise.all(textPromises);
+
+   // getting text from each cell node, and saving it with the appropriate collumn header
+   for (const text of texts) {
+      if (columnIndex !== 0) {
+         scrapedInfo.push([columnHeaders[columnIndex], text]);
+      }
+      columnIndex++;
+   }
+
+   scrapedInfo.push(tableName);
+   return scrapedInfo;
+};
+
+// data-details css attribute is a string with a ton of information used when the row is expanded to display an image of the move
+// this function parses the string for the stored src attribute which contains the image url
+// each image has 2 srcs listed. the code below correctly slices the 1st of the 2
+const parseforHref = (string) => {
+   const arr = string.split(" ");
+
+   const imgURLs = [];
+   let oddURLsOnly = true;
+   for (const str of arr) {
+      if (/^src/.test(str)) {
+         if (oddURLsOnly) {
+            imgURLs.push(`https://dustloop.com${str.slice(5, str.length - 1)}`);
+         }
+         oddURLsOnly = !oddURLsOnly;
+      }
+   }
+
+   return imgURLs;
+};
 
 // stretch goal: system data, gattlings
